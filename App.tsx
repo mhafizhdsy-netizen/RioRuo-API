85a4d5e78b3c49df86546ad563478cc08c9284f3
RioDev <mhafizhdsy@gmail.com>
2025-12-19 17:30:48 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -929,7 +929,7 @@ export function App() {
             </div>
           </div>
         </footer>
-      </header>
+      </main>
       <div className="fixed bottom-6 right-6 z-[100] flex flex-col gap-3 pointer-events-none">
         {toasts.map((toast) => (
           <div key={toast.id} className="pointer-events-auto">
